efcb9fa713202f94b7d42b9f075f64fbc280b2b7
Yeshu Kochher <yeshu@outlook.in>
2019-12-02 12:40:11 +0530

    one where add db cli commands

--- app.py
+++ app.py
@@ -3,13 +3,65 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
 
-
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, "planets.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db = SQLAlchemy(app)
+
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6
+                     )
+
+    venus = Planet(planet_name='Venus',
+                   planet_type='Class E',
+                   home_star='Sol',
+                   mass=3.258e23,
+                   radius=2516,
+                   distance=35.98e6
+                   )
+
+    earth = Planet(planet_name='Earth',
+                   planet_type='Class A',
+                   home_star='Sol',
+                   mass=4.258e23,
+                   radius=3516,
+                   distance=45.98e6
+                   )
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name='Aarav',
+                     last_name='K',
+                     email='aarav@yopmail.com',
+                     password='password')
+
+    db.session.add(test_user)
+    db.session.commit()
+
+    print('Database seeded!')
 
 
 @app.route('/')
